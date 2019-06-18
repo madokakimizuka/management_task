0dd8b0abecb5b2492ea4ae398e637e9e88187685
madoka kimizuka <kimitaro0415@gmail.com>
2019-06-18 20:29:04 +0900

    feat: ログインしていなければログイン画面へとredirectする機能を実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   http_basic_authenticate_with :name => ENV['BASIC_AUTH_USERNAME'], :password => ENV['BASIC_AUTH_PASSWORD'] if Rails.env == "production"
   protect_from_forgery with: :exception
   include SessionsHelper
+
+  before_action :login_required
+
+  private
+
+  def login_required
+    redirect_to new_user_path unless current_user
+  end
+
 end
