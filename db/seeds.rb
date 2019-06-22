eaebc2ddabef96a41903982f9ff66dceb3390ad6
madoka kimizuka <kimitaro0415@gmail.com>
2019-06-22 12:19:04 +0900

    feat: seedデータwo 作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,1 @@
-50.times do |n|
-  title = Faker::Games::Zelda.character
-  content = Faker::Games::Zelda.item
-  Task.create!(title: title,
-               content: content,
-               )
-end
-
-User.create(name: Faker::Color.color_name, email: Faker::Internet.email, password: "password", password_confirmation: "password")
+User.create!(name: Faker::Color.color_name, email: Faker::Internet.email, password: "password", password_confirmation: "password")
--- spec/features/user.spec.rb
+++ spec/features/user.spec.rb
@@ -1,17 +1,6 @@
 require 'rails_helper'
 
 RSpec.feature "ログイン機能を実装したタスク管理機能", type: :feature do
-  # background do
-  #   @user_a = FactoryBot.create(:user)
-  #   FactoryBot.create(:task, user: @user_a)
-  #   FactoryBot.create(:second_task, user: @user_a)
-  #   FactoryBot.create(:third_task, user: @user_a)
-  #   visit new_session_path
-  #   fill_in 'Email', with: 'test1@example.com'
-  #   fill_in 'Password', with: 'password'
-  #   click_on 'ログイン'
-  # end
-
   scenario "サインアップできるかのテスト" do
     visit new_user_path
     fill_in '名前', with:'user_a'
