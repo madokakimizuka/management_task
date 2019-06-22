58cb5fbcf16364bc0065a8ab8609f5c1a0b8c175
madoka kimizuka <kimitaro0415@gmail.com>
2019-06-22 14:49:47 +0900

    fix: ログインのテストが通らなかったので修正

--- spec/features/user.spec.rb
+++ spec/features/user.spec.rb
@@ -14,8 +14,8 @@ RSpec.feature "ログイン機能を実装したタスク管理機能", type: :f
   scenario "ログインできるかのテスト" do
     FactoryBot.create(:user)
     visit new_session_path
-    fill_in 'メールアドレス', with:'test1@example.com'
-    fill_in 'パスワード', with:'password'
+    fill_in 'email', with:'test1@example.com'
+    fill_in 'password', with:'password'
     click_on 'ログイン'
     expect(page).to have_content 'test1@example.com'
   end
