65215f1daef2c65ff7ef9d6ccd25bde6bfe2cfb3
madoka kimizuka <kimitaro0415@gmail.com>
2019-07-01 14:32:50 +0900

    feat: seedデータに管理者権限を持ったユーザーを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,3 @@
-User.create!(name: Faker::Color.color_name, email: Faker::Internet.email, password: "password", password_confirmation: "password")
+User.create!(name: Faker::Color.color_name, email: Faker::Internet.email, admin: true, password: "password", password_confirmation: "password")
+
+User.create!(name: Faker::Color.color_name, email: Faker::Internet.email, admin: false, password: "password", password_confirmation: "password")
